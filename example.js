6b774e063b099bc2731c42810e91782f29b2ba1e
Adrian Martinez <adrian.martinez@scholar.smashprogram.org>
2018-06-29 10:23:08 -0700

    commit

--- example.js
+++ example.js
@@ -6,18 +6,21 @@ var ballY;
 var ballRadius;
 var ballXVelocity;
 var ballGrowth;
+var ballYVelocity;
+var obstacle;
 
 // initialize global variables in setup() function
 function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("Teacher");
+  myName.html("Adrian");
   ballX = width / 2;
   ballY = height / 2;
   ballRadius = 50;
-  ballXVelocity = 10;
+  ballXVelocity = 5;
   ballGrowth = 10;
+  ballYVelocity= 5;
 }
 
 
@@ -25,20 +28,29 @@ function draw() {
   background(0, 255, 0);
   //drawSquares(random(30, 60)); // Uncomment this line for some fun!
   drawBall();
+    rect(120,130,55,55);
+    translate(30,30);
+    rect(400,430,55,55);
+    rect(340,230,20,50);
+    rect(25,290, 55, 55); 
 }
 
 function drawBall() {
   if(ballX >= width - ballRadius || ballX <= 0 + ballRadius) {
     ballXVelocity *= -1;
   }
+    if (ballY >= height-ballRadius || ballY <=0 + ballRadius) {
+        ballYVelocity *= -1;
+    } 
   var circumference = getCircumference(); // local variable
   if(circumference >= 600 || circumference <= 160) {
     ballGrowth *= -1;
   }
-  ballX += ballXVelocity;
+  ballX = ballX + ballXVelocity;
+  ballY = ballY + ballYVelocity;
   //ballRadius += ballGrowth; // Uncomment this line for a "pulsing" effect!
-  var ballDiameter = ballRadius * 2; // local variable
-  fill("orange");
+  var ballDiameter = ballRadius * 1; // local variable
+  fill("purple");
   ellipse(ballX, ballY, ballDiameter, ballDiameter);
 }
 
@@ -52,9 +64,9 @@ function drawSquares(num) {
     var randomX = random(width); // generate random float (decimal) between 0 and width - 1
     var randomY = random(height); // generate random float (decimal) between 0 and height - 1
     var randomLength = random(20, 80); // generate random float (decimal) between 20 (inclusive) and 80 (exclusive)
-    var randomRed = random(255); // amount of red is between 0 (none) and 255 (most)
-    var randomGreen = random(255);
-    var randomBlue = random(255);
+    var randomRed = random(25); // amount of red is between 0 (none) and 255 (most)
+    var randomGreen = random(125);
+    var randomBlue = random(155);
     fill(randomRed, randomGreen, randomBlue);
     rect(randomX, randomY, randomLength, randomLength);
     num--;
@@ -69,10 +81,11 @@ function drawSquaresAlt(num) {
     var randomX = random(width); // generate random float (decimal) between 0 and width - 1
     var randomY = random(height); // generate random float (decimal) between 0 and height - 1
     var randomLength = random(20, 80); // generate random float (decimal) between 20 (inclusive) and 80 (exclusive)
-    var randomRed = random(255); // amount of red is between 0 (none) and 255 (most)
-    var randomGreen = random(255);
-    var randomBlue = random(255);
+    var randomRed = random(25); // amount of red is between 0 (none) and 255 (most)
+    var randomGreen = random(55);
+    var randomBlue = random(155);
     fill(randomRed, randomGreen, randomBlue);
     rect(randomX, randomY, randomLength, randomLength);
   }
+
 }
